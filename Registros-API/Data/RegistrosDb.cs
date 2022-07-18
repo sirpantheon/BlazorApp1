aa566931c235423798beafa678f8ad2747f05352
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-07-17 23:28:29 -0300

    Initial commit

---
+++ Registros-API/Data/RegistrosDb.cs
@@ -0,0 +1,28 @@
+﻿using dataBase;
+using Microsoft.EntityFrameworkCore;
+
+namespace Registros_API.Data
+{
+    public class RegistrosDb: DbContext
+    {
+        
+        public DbSet<Registros> registro { get; set; }
+
+        public RegistrosDb(DbContextOptions<RegistrosDb> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Registros>().HasData(
+              new
+              {
+                  StudentId = Guid.NewGuid().ToString(),
+                  nome = "Jane",
+                  sobrenome = "Smith",
+                  senha = "Medicine"
+              }
+            );
+        } 
+    }
+}
---
+++ dataBase/Registros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataBase
+{
+    public class Registros
+    {
+        
+        public string nome { get; set; }
+        
+        public string sobrenome { get; set; }
+
+        public string senha { get; set; }
+
+    }
+}
